d5095d1f5a687f5d73ad7e54519b2bb0a6d721f8
Zeng Qiang <zengqiang@jxgtmbjt.com>
2022-05-10 20:00:29 +0800

    fix: 测试

--- project-name/.eslintrc.js
+++ project-name/.eslintrc.js
@@ -1,35 +1,23 @@
 module.exports = {
-  globals: {
-    __PUBLIC_PATH__: true
-  },
-  parser: "vue-eslint-parser",
-  /* 优先级低于parse的语法解析配置 */
-  parserOptions: {
-    parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
-    ecmaFeatures: {
-      tsx: true,
-      jsx: true
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
   extends: [
-    "plugin:vue/vue3-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:vue/essential',
+    'airbnb-base',
+    'plugin:prettier/recommended' // 添加 prettier 插件
   ],
-  plugins: ["vue", "prettier", "@typescript-eslint"],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
   rules: {
-    "@typescript-eslint/no-var-requires": 0,
-    "@typescript-eslint/no-explicit-any": 0,
-    "@typescript-eslint/no-empty-function": 0,
-    "@typescript-eslint/explicit-function-return-type": 0,
-    "@typescript-eslint/explicit-module-boundary-types": 0,
-    "@typescript-eslint/triple-slash-reference": 0,
-    "react/prop-types": 0,
-    "prefer-rest-params": 1,
-    "no-var": 1,
-    "vue/multi-word-component-names": 0
-  }
+  },
 };
